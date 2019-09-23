b72fa53321f147547dabaabfa82bc28112b8461b
Mitsuko Kitazawa <mitsuko.kitazawa@gmail.com>
2019-09-22 21:22:02 -0500

    solved bonus

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -3,27 +3,89 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
+  // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(`/metadata/${sample}`).then((data) => {
+    var selector = d3.select("#sample-metadata");
+      // Use `.html("") to clear any existing metadata
+    selector.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+      selector.append("h6").text(`${key}: ${value}`);
+    });
 
     // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    buildGauge(data.WFREQ);
+    console.log(data.WFREQ);
+  });
+
+
 }
+
+
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then((data) => {
+    // todo
+    const otu_ids = data.otu_ids;
+    const otu_labels = data.otu_labels;
+    const sample_values = data.sample_values;
+
+    // Build a Bubble Chart
+    var bubbleLayout = {
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" }
+    };
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        } ,
+
+        font: {
+          family: 'Arial',
+          size: 20}
+      }
+    ];
+    Plotly.plot("bubble", bubbleData, bubbleLayout);
 
     // @TODO: Build a Bubble Chart using the sample data
 
-    // @TODO: Build a Pie Chart
+    // Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+    var pieData = [
+      {
+        values: sample_values.slice(0, 10),
+        labels: otu_ids.slice(0, 10),
+        hovertext: otu_labels.slice(0, 10),
+        hoverinfo: "hovertext",
+        type: "pie"
+      }
+    ];
+
+    var pieLayout = {
+      margin: { t: 0, l: 0 },
+      font: {
+        family: 'Arial',
+        size: 200
+      },
+    };
+
+    Plotly.plot("pie", pieData, pieLayout);
+  })
+
 }
 
 function init() {
@@ -43,6 +105,7 @@ function init() {
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
+    buildGauge("");
   });
 }
 
@@ -50,6 +113,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
+  buildGauge(newSample.WFREQ);
 }
 
 // Initialize the dashboard
--- Belly_Button_Diversity/static/js/bonus.js
+++ Belly_Button_Diversity/static/js/bonus.js
@@ -1,1 +1,78 @@
+function buildGauge(wfreq){
+    //D3.json(`/samples/${sample}`).then((data) => {
+    // var selector = d3.select("#gauge");
+    // selector.html("");
+    var data = [
+        {
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wfreq,
+            title: {text:"Scrubs per week" }, 
+            font: {
+                family: 'Arial',
+                size: 14
+               
+              },
+            type: "indicator",
+            mode: "gauge+number", 
+            gauge: {
+                axis: { range: [0, 10] },
+                steps: [
+                  { range: [0, 1], color: "OldLace " },
+                  { range: [1, 2], color: "Bisque " },
+                  { range: [2, 3], color: "LavenderBlush " },
+                  { range: [3, 4], color: "Lavender  " },
+                  { range: [4, 5], color: "LightSkyBlue " },
+                  { range: [5, 6], color: "MediumTurquoise" },
+                  { range: [6, 7], color: "LightGreen" },
+                  { range: [7, 8], color: "GreenYellow" },
+                  { range: [8, 9], color: "LimeGreen" },
+                  { range: [9, 10], color: "Green" }
+                ],
+                bar: { color: "Maroon" },
+                bgcolor: "white",
+                borderwidth: 2,
+                bordercolor: "gray",
+                margin:{
+                   t:10,
+                   b:10
+                }
+            }
+        }
+    ];
 
+    var degrees = 0;
+    var radians = degrees * Math.PI / 180;
+    var x = 0;
+    var y = 0
+
+
+    var layout = { 
+        width: 500,
+        height: 400,
+        //margin: { t: 0, b: 0 } ,
+        // shapes: [{
+        //     type: 'line',
+        //     x0: 0.5,
+        //     y0: 0.45,
+        //     x1: 0.6,
+        //     y1: 0.6,
+        //     line: {
+        //       color: 'black',
+        //       width: 3
+        //     }
+        //   }],
+          title: '<b>Belly Button Washing Frequency</b>',
+          font: {
+            family: 'Arial',
+            size: 18
+          },
+        
+        //   xaxis: {visible: false, range: [-1, 1]},
+        //   yaxis: {visible: false, range: [-1, 1]}
+        }
+    
+    
+
+    Plotly.newPlot("gauge", data, layout);
+    //});
+  }
